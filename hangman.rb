74e21b2e0834e921843852fc3259b34ba8bb0688
shweta <shweta@iternia.com>
2013-08-22 19:37:11 +0530

    hangman_spec

--- hangman.rb
+++ hangman.rb
@@ -12,38 +12,9 @@ class Hangman
    end
 
    def user_input
-
    end
 
-=begin
-   def play_game
-     guess_input = gets.chomp
-     @guess_input = guess_input
-
-     if @guess_input=~ /^[a-zA-Z]$/
-
-     end
-     if @word.include? @guess_input
-       if @guess_input.length > 1
-         if (@guess_input == @word)
-           @counter = @word.length
-           result
-         end
-
-         position = @word.each_index.select{|i| convertWord[i].casecmp(guess_input) == 0}
-         position.each do |tt|
-           word_placeholder[tt] = guess_input
-           @counter +=1
-           end
-         @repeated_choices[].push(@guess_input)
-       end
-     end
-   end
-=end
   def play_game
-    #guess_input = gets.chomp
-    #@guess_input = guess_input
-
     if @repeated_choices.index(@guess_input).nil? && !(@guess_input.match(/^[[:alpha:]]+$/).nil?)
       @repeated_choices.push(@guess_input)
 
